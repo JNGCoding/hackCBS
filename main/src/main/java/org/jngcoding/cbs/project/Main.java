35faca59aaa7249c30badecaa26e4bf35641c3bc
Dhruv <homecom599@gmail.com>
2025-11-08 13:11:06 +0530

    Small Beginnings

--- main/src/main/java/org/jngcoding/cbs/project/Main.java
+++ main/src/main/java/org/jngcoding/cbs/project/Main.java
@@ -1,7 +1,37 @@
 package org.jngcoding.cbs.project;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
 public class Main {
+    public String getKey() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") + "\\secrets.txt")));
+            String line = reader.readLine();
+
+            return line.split("=")[1];
+        } catch (IOException e) {
+
+            return "Error reading file!, IOException: " + e.getMessage();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main mainObject = new Main();
+
+        Client client = Client.builder().apiKey(mainObject.getKey()).build();
+        GenerateContentResponse response =
+            client.models.generateContent(
+                "gemini-2.5-flash",
+                "Explain how AI works in a few words",
+                null
+            );
+
+        System.out.println(response.text());
     }
 }
