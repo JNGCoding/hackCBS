7656a74bdfd6fd12c58a1069ed7b98bdb8dfe2a4
Dhruv <homecom599@gmail.com>
2025-11-08 15:27:42 +0530

    Basic Doctor Interface

--- main/src/main/java/org/jngcoding/cbs/project/Main.java
+++ main/src/main/java/org/jngcoding/cbs/project/Main.java
@@ -4,34 +4,45 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
+import org.jngcoding.cbs.project.__Gemini.GeminiInterface;
+
+import com.google.api.client.http.MultipartContent;
 import com.google.genai.Client;
+import com.google.genai.types.Content;
 import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.Part;
 
 public class Main {
-    public String getKey() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") + "\\secrets.txt")));
-            String line = reader.readLine();
-
-            return line.split("=")[1];
-        } catch (IOException e) {
-
-            return "Error reading file!, IOException: " + e.getMessage();
-        }
-    }
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Main mainObject = new Main();
+        //* Get GEMINI KEY FROM SECRETS.TXT */
+        String key = "";
+        try {
+            try (BufferedReader reader = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") + "\\secrets.txt")))) {
+                String line = reader.readLine();
+                key = line.split("=")[1];
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        //************************************/
 
-        Client client = Client.builder().apiKey(mainObject.getKey()).build();
-        GenerateContentResponse response =
-            client.models.generateContent(
-                "gemini-2.5-flash",
-                "Explain how AI works in a few words",
-                null
-            );
+        GeminiInterface gemini = new GeminiInterface(key);
+        gemini.setSystemRole("You are a doctor named Healthobot who takes symtoms and give out potential diseases, risks and threats and advised solutions in minimal prompts");
 
-        System.out.println(response.text());
+        while (true) {
+            System.out.print("Enter Query: ");
+            String query = scanner.nextLine();
+            if (query.equals("exit")) {
+                break;
+            }
+
+            String response = gemini.getReponse(query);
+            System.out.println(response);
+        }
     }
 }
---
+++ main/src/main/java/org/jngcoding/cbs/project/__Gemini/GeminiInterface.java
@@ -0,0 +1,36 @@
+package org.jngcoding.cbs.project.__Gemini;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.genai.Client;
+import com.google.genai.types.Content;
+import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.Part;
+
+public class GeminiInterface {
+    private final Client client;
+    private String role = "personal AI assistant";
+    private final List<Content> ConversationHistory = new ArrayList<>();
+
+    public GeminiInterface(String key) {
+        client = Client.builder().apiKey(key).build();
+    }
+
+    public void setSystemRole(String __role__) {
+        role = __role__;
+    }
+
+    public String getSystemRole() {
+        return role;
+    }
+
+    public String getReponse(String text) {
+        Content prompt = Content.builder().role("user").parts(Part.builder().text(role + ". " + text).build()).build();
+        ConversationHistory.add(prompt);
+
+        GenerateContentResponse response = client.models.generateContent("gemini-2.5-flash", ConversationHistory, null);
+
+        return response.text();
+    }
+}
